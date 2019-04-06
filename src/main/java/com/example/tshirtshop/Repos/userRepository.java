8014acbcc4634e2a399252b399180667f737331d
Nikita Markov <foxelectro1@gmail.com>
2019-04-06 20:11:48 +0300

    Update userRepository.java

--- src/main/java/com/example/tshirtshop/Repos/userRepository.java
+++ src/main/java/com/example/tshirtshop/Repos/userRepository.java
@@ -3,6 +3,6 @@ package com.example.tshirtshop.Repos;
 import com.example.tshirtshop.Entities.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface userRepository extends CrudRepository<User, Integer> {
-    User findbyUsername(String username);
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByUsername(String username);
 }
